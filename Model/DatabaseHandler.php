a60b6ad04de2306127081a231a45d0623b9fb706
Vinnie Schelfhaut <vinnie.schelfhaut.95@hotmail.com>
2020-08-06 09:58:22 +0200

    yesterday's progress

--- Model/DatabaseHandler.php
+++ Model/DatabaseHandler.php
@@ -7,7 +7,7 @@ class DatabaseHandler
 
     public function __construct()
     {
-        $pdo = $this->openConnection();
+        $pdo = self::openConnection();
         $this->pdo = $pdo;
     }
 
@@ -27,6 +27,65 @@ class DatabaseHandler
 
         return new PDO('mysql:host=' . $dbhost . ';dbname=' . $db, $dbuser, $dbpass, $driverOptions);
     }
+
+
+    public function fetchFullCustomerList() : array
+    {
+        $pdo = self::getPdo();
+        $statement = $pdo->prepare( 'SELECT id, firstname, lastname, group_id from customer');
+        $statement->execute();
+        $customerArray = $statement->fetchAll();
+        $customers = [];
+
+        foreach($customerArray as $customer) {
+            $firstname = $customer['firstname'];
+            $lastname = $customer['lastname'];
+            $id = intval($customer['id']);
+            $groupId = intval($customer['group_id']);
+            $customers[] = new Customer($firstname, $lastname, $id, $groupId);
+
+        }
+        return $customers;
+    }
+
+    public function fetchFullProductList() :array
+    {
+        $pdo = self::getPdo();
+        $statement = $pdo->prepare('SELECT * from product');
+        $statement->execute();
+        $productArray = $statement->fetchAll();
+
+        foreach($productArray as $product) {
+            $name = $product['name'];
+            $price = intval($product['price']);
+            $id = intval($product['id']);
+            $products[] = new Product($name, $price, $id);
+        }
+        return $products;
+    }
+
+    public function fetchProductById(int $productId) : Product
+    {
+        $pdo = self::getPdo();
+        $statement = $pdo->prepare('SELECT name, price from product where id = :id');
+        $statement->bindValue('id', $productId);
+        $statement->execute();
+        $fetchArray = $statement->fetch();
+        return new Product($fetchArray['name'], $fetchArray['price'], $productId);
+
+    }
+
+    public function fetchCustomerById(int $customerId) : Customer
+    {
+        $pdo = self::getPdo();
+        $statement = $pdo->prepare('SELECT firstname, lastname, group_id from customer where id = :id');
+        $statement->bindValue('id', $customerId);
+        $statement->execute();
+        $fetchArray = $statement->fetch();
+        return new Customer($fetchArray['firstname'], $fetchArray['lastname'], $customerId, $fetchArray['group_id']);
+
+    }
+
 
     /**
      * @return PDO
--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <?php
 declare(strict_types=1);
+ini_set('display_errors','1');
+ini_set('display_startup_errors','1');
+error_reporting(E_ALL);
 
 //include all your model files here
 require 'Model/Customer.php';
 require 'Model/Product.php';
 require 'Model/CustomerGroup.php';
 require 'Model/DatabaseHandler.php';
+require 'Model/Price.php';
 
 //include all your controllers here
 require 'Controller/IndexController.php';
